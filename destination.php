80178a63b1109613e9740a75901e97409f3e5ebd
Dani <daniakowen@gmail.com>
2015-10-27 20:07:17 -0700

    Adds template files for gateway and destination
    These files will be used as templates for the gateway and destination
    pages

---
+++ destination.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<?php get_sidebar(); ?>
+
+
+<div id="content" class="two-thirds left">
+<?php if(have_posts()) : while(have_posts()) : the_post(); // start the loop ?>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>     
+        <?php the_content(); ?>
+<?php endwhile; endif; // end the loop ?>
+</div>		<!--close content-->
+<p>This page uses the destination template! (destination.php)</p>
+<p class="top-link"><a href="#nav-main">Back to top</a></p>
+
+<?php get_footer(); ?>
