9842a3cf26b98828569336dc0674f8391014fb40
Dani <daniakowen@gmail.com>
2015-11-03 20:40:52 -0800

    Created plugin and blog template files
    Adds the plugin-page.php and blog.php files to the repo

---
+++ blog.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Blog Page
+ *
+ * @package WordPress
+ * @subpackage TheWishingWellWA
+ * @since TheWishingWellWA 1.0
+ */
+?>
+<?php get_header(); ?>
+    
+<div class="page-title">
+    <div class="thumbnail-2">
+        <img src="#"/>
+    </div>
+    <div class="page-text">
+        <h1 class="title">Page Title</h1>
+        <p>Bacon ipsum dolor amet venison prosciutto hamburger, alcatra biltong tail drumstick porchetta flank shoulder. Andouille porchetta kielbasa cupim. Tongue biltong meatloaf, kielbasa pork venison pork belly pastrami corned beef t-bone pork chop landjaeger short ribs chuck.</p>
+        <p>Pork prosciutto leberkas doner tongue meatball salami pork chop ham strip steak tenderloin beef drumstick chuck. Pork chop corned beef bresaola sausage tri-tip short ribs boudin pork belly biltong hamburger ball tip.</p>
+    </div>  
+</div>    
+<article class="blog-content">
+    <h3>Title</h3>
+    <p>Bacon ipsum dolor amet venison prosciutto hamburger, alcatra biltong tail drumstick porchetta flank shoulder. Andouille porchetta kielbasa cupim.</p>
+    <p>Bacon ipsum dolor amet venison prosciutto hamburger, alcatra biltong tail drumstick porchetta flank shoulder. Andouille porchetta kielbasa cupim.</p>
+    <p>Bacon ipsum dolor amet venison prosciutto hamburger, alcatra biltong tail drumstick porchetta flank shoulder. Andouille porchetta kielbasa cupim.</p>
+</article>    
+
+<?php get_sidebar(); ?>
+
+</div>		<!--close content-->
+
+<p>This page uses the blog template! (blog.php)</p>
+<p class="top-link"><a href="#nav-main">Back to top</a></p>
+
+<?php get_footer(); ?>
