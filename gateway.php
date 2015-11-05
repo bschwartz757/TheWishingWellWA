86859074c116a5190bb8c7e84d81e987b9eacf34
Dani <daniakowen@gmail.com>
2015-11-05 12:51:39 -0800

    Adds content to gateway.php and style.css
    Creates placeholder content for the two pages that use gateway.php and
    general website edits

--- gateway.php
+++ gateway.php
@@ -9,60 +9,61 @@
 ?>
 <?php get_header(); ?>
 
-<div class="slider">
+<div class="spotlight">
     <h1>Learn More</h1>
 </div>
 
-<div class="about">
+<div class="gateway-main-tile">
     <h2>About</h2>
     <p>Bacon ipsum dolor amet venison prosciutto hamburger, alcatra biltong tail drumstick porchetta flank shoulder. Andouille porchetta kielbasa cupim. Tongue biltong meatloaf, kielbasa pork venison pork belly pastrami corned beef t-bone pork chop landjaeger short ribs chuck. Pork prosciutto leberkas doner tongue meatball salami pork chop ham strip steak tenderloin beef drumstick chuck. Pork chop corned beef bresaola sausage tri-tip short ribs boudin pork belly biltong hamburger ball tip.</p>
-</div>
+</div> <!--end gateway-main-tile-->
 
 <?php get_sidebar(); ?>
-
-<div class="section main-thumbnails">
-    <div class="row-1">
-        <div class="thumbnail-3">
-            <a href="#"><img src="#"/>Img 1</a>
+<div class="clear"></div>
+<div class="section-tiles">
+    <div class="gateway-row-1">
+        <div class="tile-1">
+            <a href="#"></a>
             <div class="caption">
                 <h4>Mission</h4>
             </div>
         </div>
-        <div class="thumbnail-3">
-            <a href="#"><img src="#"/>Img 2</a>  
+        <div class="tile-2">
+            <a href="#"></a>  
             <div class="caption">
                 <h4>History</h4>
             </div>            
         </div>
-        <div class="thumbnail-3">
-            <a href="#"><img src="#"/>Img 3</a>
+        <div class="tile-3">
+            <a href="#"></a>
             <div class="caption">
                 <h4>Staff</h4>
             </div>            
         </div>        
-    </div>
-    <div class="row-2">
-        <div class="thumbnail-3">
-            <a href="#"><img src="#"/>Img 1</a>
+    </div> <!-- end gateway-row-1-->
+    <div class="clear"></div>
+    <div class="gateway-row-2">
+        <div class="tile-4">
+            <a href="#"></a>
             <div class="caption">
                 <h4>Testimonials</h4>
             </div>            
         </div>
-        <div class="thumbnail-3">
-            <a href="#"><img src="#"/>Img 2</a>
+        <div class="tile-5">
+            <a href="#"></a>
             <div class="caption">
                 <h4>Funding</h4>
             </div>            
         </div>
-        <div class="thumbnail-3">
-            <a href="#"><img src="#"/>Img 3</a>
+        <div class="tile-6">
+            <a href="#"></a>
             <div class="caption">
-                <h4>Contact</h4>
+                <h4>Partners</h4>
             </div>            
         </div>        
-    </div>
-</div> 
-
+    </div> <!-- end gateway-row-2 -->
+</div> <!--end section-tiles-->
+<div class="clear"></div>
 </div> <!--close content-->    
     
 <p>This page uses the gateway template! (gateway.php)</p>
