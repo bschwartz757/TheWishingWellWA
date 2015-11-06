f269ea5854745336d8f652e25f9738e0b069d9a6
bschwartz757 <blakehschwartz@gmail.com>
2015-11-05 21:03:45 -0800

    Minor changes to destination and style

--- destination.php
+++ destination.php
@@ -10,15 +10,16 @@
 <?php get_header(); ?>
     
 <div class="destination-page-title">
+    <div class="destination-title-img">
+            <?php the_post_thumbnail('medium'); ?>
+    </div>
     <div class="title-text">
-        <h1 class="title">Page Title</h1>
-        <p>Plaid you probably haven't heard of them cardigan, irony tote bag yuccie cronut XOXO gastropub pop-up. Flexitarian williamsburg mixtape single-origin coffee, kickstarter 3 wolf moon plaid jean shorts. YOLO brunch cray four dollar toast. Vice helvetica celiac neutra. Celiac cliche biodiesel, gastropub fashion axe mustache direct trade. 3 wolf moon humblebrag chicharrones scenester hoodie truffaut, actually marfa tattooed single-origin coffee. Selfies migas lomo hammock single-origin coffee.</p>
+        <h1 class="title"><?php the_title(); ?></h1>
+        <?php the_excerpt(); ?>
     </div>  
 </div>    
 <article class="destination-content">
-    <p>Plaid you probably haven't heard of them cardigan, irony tote bag yuccie cronut XOXO gastropub pop-up. Flexitarian williamsburg mixtape single-origin coffee, kickstarter 3 wolf moon plaid jean shorts. YOLO brunch cray four dollar toast. Vice helvetica celiac neutra. Celiac cliche biodiesel, gastropub fashion axe mustache direct trade.</p>
-    
-    <p>Art party roof party four dollar toast, food truck cray cronut banh mi pitchfork. Butcher 3 wolf moon bitters aesthetic tattooed. Pour-over shabby chic try-hard, tofu retro jean shorts ethical crucifix yr farm-to-table neutra lumbersexual drinking vinegar 90's distillery. Chartreuse put a bird on it swag plaid. Irony tote bag keytar single-origin coffee banjo master cleanse, kale chips mlkshk umami seitan waistcoat artisan typewriter.</p>
+    <?php the_content(); ?>
 </article>    
 
 <?php get_sidebar(); ?>
