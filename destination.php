c845ea5bb3ba6bbea7cb6877d60888a82877ab83
bschwartz757 <blakehschwartz@gmail.com>
2015-11-05 20:04:50 -0800

    Adds placeholder image

--- destination.php
+++ destination.php
@@ -10,9 +10,6 @@
 <?php get_header(); ?>
     
 <div class="destination-page-title">
-    <div class="thumbnail-med">
-        <img src="#" />
-    </div>
     <div class="title-text">
         <h1 class="title">Page Title</h1>
         <p>Plaid you probably haven't heard of them cardigan, irony tote bag yuccie cronut XOXO gastropub pop-up. Flexitarian williamsburg mixtape single-origin coffee, kickstarter 3 wolf moon plaid jean shorts. YOLO brunch cray four dollar toast. Vice helvetica celiac neutra. Celiac cliche biodiesel, gastropub fashion axe mustache direct trade. 3 wolf moon humblebrag chicharrones scenester hoodie truffaut, actually marfa tattooed single-origin coffee. Selfies migas lomo hammock single-origin coffee.</p>
