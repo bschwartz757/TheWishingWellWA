af1f06fcfed855bd32a63b7545ae117c4e555935
bschwartz757 <blakehschwartz@gmail.com>
2015-11-10 22:58:47 -0800

    Fixes wp footer nav

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-<!-- Start conventional footer -->
+<!-- Start footer -->
 <footer>
  
 <div class="social-icons">
@@ -13,28 +13,16 @@
 <div class="donate-button">
     <button type="submit" class="btn-success">Donate</button>
 </div>  
-<div class="clear"></div>
-<!--<div class="footer-nav">
-    <ul>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Volunteer</a></li>
-        <li><a href="#">Donate</a></li>
-        <li><a href="#">Events</a></li>
-        <li><a href="#">Photos</a></li>
-        <li><a href="#">Resources</a></li>        
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Contact</a></li>        
-    </ul>
-</div> -->   
+<div class="clear"></div>  
     
 <!-- Start wp Footer Menu -->
-        <!-- <?php var_dump($post) ?> -->
-<?php wp_nav_menu( array('theme_location' => 'footer-menu','container' => 'div','container_id' => 'links')); ?>
+
+<?php wp_nav_menu( array('theme_location' => 'footer-menu','container' => 'nav','container_id' => 'nav-footer')); ?>
 <!-- End wp Footer Menu -->
 
     <p>&copy; The Wishing Well 2015 - <?php print("" . date('Y') . ""); ?></p>
 </footer>
-<!-- close conventional footer -->
+<!-- close footer -->
 
 <!-- Start WP Footer - contains js scripts, etc. -->
 <?php wp_footer(); ?>
