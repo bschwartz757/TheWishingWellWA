04a5370d279ffd80e0e6c30acb67c14b943f7482
bschwartz757 <blakehschwartz@gmail.com>
2015-11-17 20:59:24 -0800

    Slight changes to blog template
    Replaces the_content with the_excerpt

--- index.php
+++ index.php
@@ -12,10 +12,9 @@
 <article class="blog-excerpt">
     <?php if(have_posts()) : while(have_posts()) : the_post(); //start the loop ?>
     <h2><a href="<?php the_permalink();// links to the post ?>"><?php the_title();// gets the post title ?></a></h2>
-    <p><?php the_content(); ?></p>
-    <small>Posted on <?php the_time('F j, Y');// gets the time ?> by <?php the_author();// gets the author ?> in <?php the_category(', ');// gets the category - comma separates if multiple ?></small>                            
-    <?php the_post_thumbnail('thumbnail');// gets the post thumbnail ?>          
-    <?php the_excerpt();// gets the post excerpt ?>                              
+    <p><?php the_excerpt();// gets the post excerpt ?></p>
+    <?php the_post_thumbnail('thumbnail');// gets the post thumbnail ?>
+    <small>Posted on <?php the_time('F j, Y');// gets the time ?> by <?php the_author();// gets the author ?> in <?php the_category(', ');// gets the category - comma separates if multiple ?></small>                         
     <p class="read-more"><a href="<?php the_permalink(); ?>">Read More</a></p>
     <?php endwhile; else : ?>
     <p><?= ('Sorry, no posts matched your criteria.'); ?></p>
