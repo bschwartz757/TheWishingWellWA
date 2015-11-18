84ed675acd7fcddfa978d4c186f02862ede3d219
bschwartz757 <blakehschwartz@gmail.com>
2015-11-17 20:45:03 -0800

    Corrects missing content issues
    Uses post_content() to get content for blog and destination pages

--- index.php
+++ index.php
@@ -9,15 +9,19 @@
 ?>
 <?php get_header(); ?>
 
-        <?php if(have_posts()) : while(have_posts()) : the_post(); //start the loop ?>
-        	<article class="post-excerpt">
-        		<h2><a href="<?php the_permalink(); ?>"<?php the_title(); ?>></a></h2>
-                <a href="<?php the_permalink(); ?>"><?= get_the_post_thumbnail($post->ID, 'thumbnail'); ?></a><?php the_excerpt(); ?>
-                <p class="read-more"><a href="<?php the_permalink(); ?>">Read More</a></p>
-        	</article>
-        <?php endwhile; else : ?>
-        	<p><?= ('Sorry, no posts matched your criteria.'); ?></p>
-        <?php endif; ?>
+<article class="blog-excerpt">
+    <?php if(have_posts()) : while(have_posts()) : the_post(); //start the loop ?>
+    <h2><a href="<?php the_permalink();// links to the post ?>"><?php the_title();// gets the post title ?></a></h2>
+    <p><?php the_content(); ?></p>
+    <small>Posted on <?php the_time('F j, Y');// gets the time ?> by <?php the_author();// gets the author ?> in <?php the_category(', ');// gets the category - comma separates if multiple ?></small>                            
+    <?php the_post_thumbnail('thumbnail');// gets the post thumbnail ?>          
+    <?php the_excerpt();// gets the post excerpt ?>                              
+    <p class="read-more"><a href="<?php the_permalink(); ?>">Read More</a></p>
+    <?php endwhile; else : ?>
+    <p><?= ('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
+</article> 
+
     	</div><!--Close content-->
 
 <?php get_sidebar(); ?>                
