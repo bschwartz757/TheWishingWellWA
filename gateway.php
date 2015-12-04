a278c7837b5a68263c50bdb6148d56c5afdbdb58
Dani <daniakowen@gmail.com>
2015-12-03 19:56:13 -0800

    Fixes gateway page dynamic content display
    Ensured gateway.php page title, content and child pages are created
    dynamically from WordPress

--- gateway.php
+++ gateway.php
@@ -15,51 +15,16 @@
 </div>
 
 <div class="gateway-main-tile">
-    <h2>About</h2>
-    <p>Bacon ipsum dolor amet venison prosciutto hamburger, alcatra biltong tail drumstick porchetta flank shoulder. Andouille porchetta kielbasa cupim. Tongue biltong meatloaf, kielbasa pork venison pork belly pastrami corned beef t-bone pork chop landjaeger short ribs chuck. Pork prosciutto leberkas doner tongue meatball salami pork chop ham strip steak tenderloin beef drumstick chuck. Pork chop corned beef bresaola sausage tri-tip short ribs boudin pork belly biltong hamburger ball tip.</p>
+    <h2><?php the_title(); ?></h2>
+    <p><?php if(have_posts()) : the_post(); ?>
+ <?php the_content(); ?>
+    <?php endif; ?></p>
 </div> <!--end gateway-main-tile-->
 
 <?php get_sidebar(); ?>
 <!--<div class="clear"></div>-->
 <div class="section-tiles">
-<?php get_child_pages(); ?>
-
-        <!-- <div class="tile first">
-           <img src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/kitty_300_300.jpg" alt="tile image"/>
-            <div class="caption">
-              <a href="<?php echo home_url(); ?>/?page_id=64"><h1>Mission &gt;&gt;</h1></a>
-            </div>
-        </div>
-        <div class="tile second"> 
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/puppy_300_300.jpg" alt="tile image"/>
-            <div class="caption">
-                <a href="<?php echo home_url(); ?>/?page_id=66"><h1>History &gt;&gt;</h1></a>
-            </div>            
-        </div>
-        <div class="tile third">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/kitty_300_300.jpg" alt="tile image"/>
-            <div class="caption">
-                <a href="<?php echo home_url(); ?>/?page_id=68"><h1>Staff &gt;&gt;</h1></a>
-            </div>            
-        </div>        
-        <div class="tile first">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/puppy_300_300.jpg" alt="tile image"/>
-            <div class="caption">
-                <a href="<?php echo home_url(); ?>/?page_id=20"><h1>Testimonials &gt;&gt;</h1></a>
-            </div>            
-        </div>
-        <div class="tile second">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/kitty_300_300.jpg" alt="tile image"/>
-            <div class="caption">
-                <a href="<?php echo home_url(); ?>/?page_id=22"><h1>Funding &gt;&gt;</h1></a>
-            </div>            
-        </div>
-        <div class="tile third">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/puppy_300_300.jpg" alt="tile image"/>
-            <div class="caption">
-                <a href="<?php echo home_url(); ?>/?page_id=24"><h1>Partners &gt;&gt;</h1></a>
-            </div>            
-        </div> -->        
+<?php get_child_pages(); ?>       
  
 </div> <!--end section-tiles-->
 <!--<div class="clear"></div>-->
--- plugin-page.php
+++ plugin-page.php
@@ -8,17 +8,10 @@
  */
 ?>
 <?php get_header(); ?>
-    
-<div class="page-title">
-    <div class="thumbnail-2">
-    </div>
-    <div class="page-text">
-        <h1 class="title"><?php the_title(); ?></h1>
-        
-    </div>  
-</div>    
+       
 <article class="plugin-content">
     <h3><?php the_title(); ?> Template</h3>
+
 </article>    
 
 <?php get_sidebar(); ?>
